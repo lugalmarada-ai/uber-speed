ff9e3057538b112f3e18a22d539b275564dfcf7a
@BlockFusion_Tech <josevicente.reyes@gmail.com>
2026-01-07 15:36:14 +0000

    fix: Use port 80/Proxy for API to bypass Firewall blocking port 3000

--- app/src/main/java/com/uberspeed/client/utils/Constants.kt
+++ app/src/main/java/com/uberspeed/client/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.uberspeed.client.utils
 
 object Constants {
-    const val BASE_URL = "http://54.67.104.47:3000/api/"
+    const val BASE_URL = "http://54.67.104.47/api/"
     const val PREFS_NAME = "uber_speed_prefs"
 }
